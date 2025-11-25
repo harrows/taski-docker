7b2859a152b9e734801e5a190a04260546b460d0
Дмитрий Машонский <harrows2012@gmail.com>
2025-11-25 14:13:46 +0300

    docker_learn_2

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -8,7 +8,7 @@ SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['localhost', '123.123.123.123', '<your_domain>']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '51.250.107.102', 'myprakt2025.zapto.org']
 
 
 # Application definition
